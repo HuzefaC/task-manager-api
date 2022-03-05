724865c0cd4469eee8c62fcce53ce5b40dcf8708
HuzefaC <huzefachabukswar@gmail.com>
2022-03-06 01:35:15 +0530

    Bug fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
-const env = require('env-cmd');
 require('./db/mongoose');
 
 const app = express();
